8d433ed844839adfb96579d3fb308d587385fc37
Mohammad Romin <63338660+mohammadromin03@users.noreply.github.com>
2020-07-21 21:16:49 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+	 // var firstName = "<h2>Mohammad</h2> ";
+  // var lastName = "<h2>Romin</h2>";
+  // var fullName = firstName + lastName;
+
+  // document.write(fullName);
+
+
+
+    // document.write("Mohammad " + "Romin");
+
